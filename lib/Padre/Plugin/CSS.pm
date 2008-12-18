a8968a4f29135e14d35656e2bd8aa4e03a6effff
Fayland 林 <fayland@gmail.com>
2008-12-18 10:28:35 +0000

    Import of FAYLAND/Padre-Plugin-CSS-0.01 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-CSS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FAYLAND/Padre-Plugin-CSS-0.01.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/Padre/Plugin/CSS.pm
@@ -0,0 +1,85 @@
+package Padre::Plugin::CSS;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+use base 'Padre::Plugin';
+use Wx ':everything';
+
+sub padre_interfaces {
+	'Padre::Plugin' => '0.21',
+}
+
+sub menu_plugins_simple {
+	'CSS' => [
+		'Validate CSS',  \&validate_css,
+	];
+}
+
+sub validate_css {
+	my ( $self ) = @_;
+	
+	my $doc  = $self->selected_document;
+	my $code = $doc->text_get;
+	
+	unless ( $code and length($code) ) {
+		Wx::MessageBox( 'No Code', 'Error', Wx::wxOK | Wx::wxCENTRE, $self );
+	}
+	
+	require WebService::Validator::CSS::W3C;
+	my $val = WebService::Validator::CSS::W3C->new();
+	my $ok  = $val->validate(string => $code);
+
+	if ($ok) {
+		if ( $val->is_valid ) {
+			_output( $self, "CSS is valid\n" );
+		} else {
+			my $error_text = "CSS is not valid\n";
+			$error_text .= "Errors:\n";
+			my @errors = $val->errors;
+			foreach my $err (@errors) {
+				my $message = $err->{message};
+				$message =~ s/(^\s+|\s+$)//isg;
+				$error_text .= " * $message ($err->{context}) at line $err->{line}\n";
+			}
+			_output( $self, $error_text );
+		}
+	} else {
+		my $error_text = sprintf("Failed to validate the code\n");
+        _output( $self, $error_text );
+	}
+}
+
+sub _output {
+	my ( $self, $text ) = @_;
+	
+	$self->show_output;
+	$self->{gui}->{output_panel}->clear;
+	$self->{gui}->{output_panel}->AppendText($text);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Padre::Plugin::CSS - L<Padre> and CSS
+
+=head1 Validate CSS
+
+use L<WebService::Validator::CSS::W3C> to validate the CSS
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
